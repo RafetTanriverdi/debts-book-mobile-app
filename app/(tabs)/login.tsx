6707824bf0f5d89f1ba89ec77a40cfd02db47213
RafetTanriverdi <by.rafet.tanriverdi@gmail.com>
2025-05-12 23:01:42 +0300

    login process added

---
+++ app/(tabs)/login.tsx
@@ -0,0 +1,119 @@
+import { supabase } from "@/lib/supabase";
+import React, { useState } from "react";
+import {
+  Alert,
+  StyleSheet,
+  View,
+} from "react-native";
+import {
+  Button,
+  TextInput as Input,
+} from "react-native-paper";
+import 'react-native-url-polyfill/auto';
+
+export default function Auth() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  async function signInWithEmail() {
+    setLoading(true);
+    const { error ,data} =
+      await supabase.auth.signInWithPassword({
+        email: email,
+        password: password,
+      });
+
+      console.log("data", data);
+    if (error) Alert.alert(error.message);
+    setLoading(false);
+  }
+
+  async function signUpWithEmail() {
+    setLoading(true);
+    const {
+      data: { session },
+      error,
+    } = await supabase.auth.signUp({
+      email: email,
+      password: password,
+    });
+
+    if (error) Alert.alert(error.message);
+    if (!session)
+      Alert.alert(
+        "Please check your inbox for email verification!"
+      );
+    setLoading(false);
+  }
+
+  return (
+    <View style={styles.container}>
+      <View
+        style={[
+          styles.verticallySpaced,
+          styles.mt20,
+        ]}
+      >
+        <Input
+          label="Email"
+      
+          onChangeText={(text) => setEmail(text)}
+          value={email}
+          placeholder="email@address.com"
+          autoCapitalize={"none"}
+        />
+      </View>
+      <View style={styles.verticallySpaced}>
+        <Input
+          label="Password"
+          
+          onChangeText={(text) =>
+            setPassword(text)
+          }
+          value={password}
+          secureTextEntry={true}
+          placeholder="Password"
+          autoCapitalize={"none"}
+        />
+      </View>
+      <View
+        style={[
+          styles.verticallySpaced,
+          styles.mt20,
+        ]}
+      >
+        <Button
+          disabled={loading}
+          onPress={() => signInWithEmail()}
+        >
+          Sign in
+        </Button>
+      </View>
+      <View style={styles.verticallySpaced}>
+        <Button
+  
+          disabled={loading}
+           onPress={() => signUpWithEmail()}
+        >
+          Sign up
+        </Button>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 40,
+    padding: 12,
+  },
+  verticallySpaced: {
+    paddingTop: 4,
+    paddingBottom: 4,
+    alignSelf: "stretch",
+  },
+  mt20: {
+    marginTop: 20,
+  },
+});
